ede6d44ede0cec614557ebfce726cfe35db4f933
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-06-20 23:17:22 +0900

    feat: QR 페이지 제작
    카메라 사이즈는 추후에 조정 해야함

--- src/app/main/qrRead/page.jsx
+++ src/app/main/qrRead/page.jsx
@@ -5,19 +5,22 @@ import { QrReader } from 'react-qr-reader';
 const Page = () => {
     const [data, setData] = useState('No result');
     return (
-        <QrReader
-            onResult={(result, error) => {
-                if (!!result) {
-                    setData(result?.text);
-                }
+        <Flex alignItems='center' justifyContent='center' height='100%' >
+            <Box width='80%' >
+                <QrReader
+                    onResult={(result, error) => {
+                        if (!!result) {
+                            setData(result?.text);
+                        }
 
-                if (!!error) {
-                    console.info(error);
-                }
-            }}
-            style={{ width: '100%' }}
-            videoStyle={{ border: '5px solid #00FF0A' }}
-        />
+                        if (!!error) {
+                            console.info(error);
+                        }
+                    }}
+                    videoStyle={{ border: '5px solid #00FF0A' }}
+                />
+            </Box>
+        </Flex>
     )
 }
 
