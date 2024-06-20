6b9dd0d83b9999a004d9a1af0138c66ce7ff5905
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-06-20 23:16:44 +0900

    feat: path 속성 추가

--- src/app/main/_data/data.jsx
+++ src/app/main/_data/data.jsx
@@ -11,25 +11,29 @@ export const seminar = [
         id: 0,
         color: '#EA4336',
         icon: <Check />,
-        text: '세미나 출석체크'
+        text: '세미나 출석체크',
+        path: '/attendance'
     },
     {
         id: 1,
         color: '#FA7800',
         icon: <Support />,
-        text: '세미나 발표 지원'
+        text: '세미나 발표 지원',
+        path: '/attendance'
     },
     {
         id: 2,
         color: '#FA397E',
         icon: <Speaker />,
-        text: '세미나 연사자 확인'
+        text: '세미나 연사자 확인',
+        path: '/attendance'
     },
     {
         id: 3,
         color: '#9B36EA',
         icon: <Absence />,
-        text: '세미나 불참 제출'
+        text: '세미나 불참 제출',
+        path: '/attendance'
     },
 ]
 
