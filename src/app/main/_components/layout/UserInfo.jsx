c225ebf7ca38ee4f6c5696595578d36f44fa88b2
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-06-20 23:16:32 +0900

    style: padding 조정

--- src/app/main/_components/layout/UserInfo.jsx
+++ src/app/main/_components/layout/UserInfo.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const UserInfo = () => {
     return (
-        <Flex direction='column' padding='54px 34px 54px 34px'>
+        <Flex direction='column' padding='20px 34px 35px 34px' bg={'transparent'} >
             <Text color='#E6E6E6' fontSize='18px'>{'Lead'}</Text>
             <Heading color='#ffffff' fontSize='28px'>{'정상현님'} 안녕하세요</Heading>
         </Flex>
