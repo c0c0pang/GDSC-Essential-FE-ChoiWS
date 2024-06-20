f0208a3315f171f3a133ed919600b6b192db3d9f
c0c0pang <91197026+c0c0pang@users.noreply.github.com>
2024-06-20 23:15:55 +0900

    feat: 각 버튼을 누르면 해당 하는 페이지로 이동하는 기능 구현
    QR 페이지만 적용

--- src/app/main/_components/layout/Seminar.jsx
+++ src/app/main/_components/layout/Seminar.jsx
@@ -1,33 +1,40 @@
 import { Box, Flex, Grid, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 import { seminar } from '../../_data/data'
+import { usePathname } from 'next/navigation'
+import Link from 'next/link'
 
 const Seminar = () => {
+    const nowPath = usePathname();
     return (
-        <Flex direction='column' padding='34px 34px 0px 34px' >
+        <Flex direction='column' padding='34px 34px 0px 34px'>
             <Box>
-                <Heading color='#000000' fontSize='3xl'>{'세미나'}</Heading>
-                <Text color='#A1A1A1' fontSize='12px'>{'세미나와 관련된 기능들입니다.'}</Text>
+                <Heading color='#000000' fontSize='2xl'>{'세미나'}</Heading>
+                <Text color='#A1A1A1' fontSize='10px'>{'세미나와 관련된 기능들입니다.'}</Text>
             </Box>
             <Grid templateColumns='repeat(4, 1fr)' gap={2} marginTop='15px'>
                 {seminar.map((e) => {
                     return (
-                        <Flex
-                            key={e.id}
-                            bg={e.color}
-                            alignItems='center'
-                            justifyContent='space-around'
-                            flexDirection='column'
-                            padding={2}
-                            borderRadius={10}
-                            h='8dvh'
-                            w='100%'
-                            shadow='0px 4px 4px 0px #00000040'
-                            cursor='pointer'
-                        >
-                            {e.icon}
-                            <Text color='#FFFFFF' fontSize='7px'>{e.text}</Text>
-                        </Flex>)
+                        <Link href={`${nowPath}/${e.path}`} key={e.id}>
+                            <Flex
+                                bg={e.color}
+                                alignItems='center'
+                                justifyContent='space-around'
+                                flexDirection='column'
+                                padding={2}
+                                borderRadius={10}
+                                h='9.5lvh'
+                                w='100%'
+                                shadow='0px 4px 4px 0px #00000040'
+                                cursor='pointer'
+                            >
+                                <Box>
+                                    {e.icon}
+                                </Box>
+                                <Text color='#FFFFFF' fontSize='7px'>{e.text}</Text>
+                            </Flex>
+                        </Link>
+                    )
                 })}
             </Grid>
         </Flex>
